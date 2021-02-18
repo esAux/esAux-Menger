017feba4e9d923fce5fb23ee2c6eb9a3636d0e6c
esAux <75986423+esAux@users.noreply.github.com>
2021-02-18 21:09:25 +0000

    Update menger.c

--- menger.c
+++ menger.c
@@ -397,7 +397,6 @@ int main(int argc, char** argv)
 
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
-    glDisable(GL_TEXTURE_2D);
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
